193ba030eae3a1f4f51ffecf813c3e6a76b90363
Tamar Zanzouri <zanzouritamar@gmail.com>
2015-01-17 14:50:05 +0200

    second commit
    form counting results + moving to static party page

--- js/calcalistFunctions.js
+++ js/calcalistFunctions.js
@@ -1,8 +1,12 @@
 var sectionId;
 var q_num = 1;
 var answers=[];
+var chkimAns = [0, 0, 0, 0, 0, 0, 0, 0];
+var result = ["licod", "avoda", "yeshAtid", "abityehudi", "culanu", "israelbitnu", "mertz", "hadashe"];
+var i;
 var url = "partyResult/partyResult.html";
 $(document).ready(function() {
+	$("section#partyPage").hide();
 	$('section.questions:not(:first)').hide();
 	sectionId = $('section.questions').attr('id');
 });
@@ -17,11 +21,65 @@ function checkform(obj){
 	if (parseInt(q_num,10)==12){
 		alert("from end");
 		console.log(answers);
-		$(location).attr('href',url);
-		return false;
+		checkAns();
+		//return false;
 	}
 	sectionId = $("#"+sectionId).next().attr("id");
 	$("section.questions").hide();
    	$("#"+sectionId).show();
 	return false;
+}
+
+function checkAns() {
+	console.log("in check answers");
+	$.getJSON("js/answer.json", function(data) {
+		var dataAnswer = data.Answers;
+		$.each(dataAnswer, function(index, value) {
+			//parties.push(Party(value.name, value.id, value.number_of_seats, value.members))
+			console.log(value.licod);
+			if (answers[index] != value.licod) {
+				chkimAns[0]++;
+			}
+			if (answers[index] != value.avoda) {
+				chkimAns[1]++;
+			}
+			if (answers[index] != value.yeshAtid) {
+				chkimAns[2]++;
+			}
+			if (answers[index] != value.abityehudi) {
+				chkimAns[3]++;
+			}
+			if (answers[index] != value.culanu) {
+				chkimAns[4]++;
+			}
+			if (answers[index] != value.israelbitnu) {
+				chkimAns[5]++;
+			}
+			if (answers[index] != value.mertz) {
+				chkimAns[6]++;
+			}
+			if (answers[index] != value.hadashe) {
+				chkimAns[7]++;
+			}
+		});
+		console.log(chkimAns+" המרחק מהתשובות של המפלגות:\n");
+		var index = 0, smaller = 10;
+		for ( i = 0; i < 8; i++)
+			if (smaller > chkimAns[i]) {
+				smaller = chkimAns[i];
+				//index = i;
+			}
+		for ( i = 0; i < 8; i++)
+			if (smaller==chkimAns[i]) {
+				console.log(result[i]);
+			}
+		console.log(chkimAns);
+		moveToPartyPage();
+	});
+}
+
+function moveToPartyPage(){
+	console.log("in party page");
+	$("section#formContainer").hide();
+	$("section#partyPage").show();
 }
